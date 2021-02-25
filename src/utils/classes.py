28ea941422f7f7a593c6e92731173f7e0579cfba
minibox24 <minibox724@gmail.com>
2021-02-25 11:34:59 +0900

    feat: add join_time, check_time

--- src/utils/classes.py
+++ src/utils/classes.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 
 
 class GameUser:
@@ -6,7 +7,11 @@ class GameUser:
         self.id: str = userid
         self.con = con
         self.cur = con.cursor()
+
         self.money = None
+        self.join_time = None
+        self.check_time = None
+
         self.load()
 
     @staticmethod
@@ -23,7 +28,10 @@ class GameUser:
     @staticmethod
     def join(con: sqlite3.Connection, userid: str, gift: int = 10000) -> None:
         cur = con.cursor()
-        cur.execute("INSERT INTO user VALUES(?, ?)", (userid, str(gift)))
+        cur.execute(
+            "INSERT INTO user VALUES(?, ?, ?, ?)",
+            (userid, str(gift), str(time.time()), "0.0"),
+        )
         con.commit()
 
     def remove(self) -> None:
@@ -37,9 +45,12 @@ class GameUser:
 
         if data:
             self.money = data[1]
+            self.join_time = float(data[2])
+            self.check_time = float(data[3])
 
     def commit(self):
         self.cur.execute(
-            "UPDATE user SET money=? WHERE id=?", (str(self.money), self.id)
+            "UPDATE user SET money=? check_time=? WHERE id=?",
+            (str(self.money), str(self.check_time), self.id),
         )
         self.con.commit()
