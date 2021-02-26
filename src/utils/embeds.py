d0e694771777c39ed0dec3e8145356b700ebe922
minibox24 <minibox724@gmail.com>
2021-02-27 01:16:12 +0900

    feat: make_text_embed title

--- src/utils/embeds.py
+++ src/utils/embeds.py
@@ -2,8 +2,11 @@ import discord
 from src.utils.colors import GREEN
 
 
-def make_text_embed(user: discord.User, text: str, color: int = GREEN) -> discord.Embed:
+def make_text_embed(
+    user: discord.User, text: str, color: int = GREEN, title: str = None
+) -> discord.Embed:
     embed = discord.Embed(color=color)
+    embed.title = title
     embed.description = text
     embed.set_footer(text=user.display_name, icon_url=user.avatar_url)
     return embed
