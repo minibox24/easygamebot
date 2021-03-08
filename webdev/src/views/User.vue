ea1215065f43d825196caea5581fa3b3c742de07
minibox24 <minibox724@gmail.com>
2021-03-08 10:15:47 +0900

    feat(web): user table

--- webdev/src/views/User.vue
+++ webdev/src/views/User.vue
@@ -1,14 +1,156 @@
 <template>
   <div id="user">
-    User Page
+    <table id="user-table">
+      <thead>
+        <tr>
+          <th>유저</th>
+          <th>잔액</th>
+          <th>가입일</th>
+          <th>출석체크일</th>
+          <th>액션</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="userbox" v-for="user in users" :key="user">
+          <td class="user">
+            <b-avatar class="mr-1" :src="user.avatar" size="16px"/>
+            <span>{{ user.name.length > 16 ? `${user.name.slice(0, 15)}...` : user.name }}</span>
+            <span class="discord-tag">#{{ user.tag }}</span>
+          </td>
+          <td>{{ user.money }}{{ unit }}</td>
+          <td>{{ user.joinAt }}</td>
+          <td>{{ user.checkAt }}</td>
+          <td>
+            <b-icon
+              icon="info-circle-fill" variant="info"
+              class="mr-1 iconbtn" v-b-tooltip.v-light.hover.top="'상세 정보'"
+            />
+            <b-icon
+              icon="trash-fill" variant="danger"
+              class="iconbtn" v-b-tooltip.v-light.hover.top="'삭제'"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      users: [
+        { avatar: '', name: 'Minibsdfgsdfgsdfgdox', tag: '3332', money: '123', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: '붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸', tag: '3332', money: '12344', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mindfgibox', tag: '3332', money: '124443', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mini', tag: '3332', money: '1423', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mdfginibox', tag: '3332', money: '1243', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mbox', tag: '3332', money: '14444423', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Minibsdfgsdfgsdfgdox', tag: '3332', money: '123', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: '붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸', tag: '3332', money: '12344', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mindfgibox', tag: '3332', money: '124443', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mini', tag: '3332', money: '1423', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mdfginibox', tag: '3332', money: '1243', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mbox', tag: '3332', money: '14444423', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Minibsdfgsdfgsdfgdox', tag: '3332', money: '123', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: '붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸', tag: '3332', money: '12344', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mindfgibox', tag: '3332', money: '124443', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mini', tag: '3332', money: '1423', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mdfginibox', tag: '3332', money: '1243', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mbox', tag: '3332', money: '14444423', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Minibsdfgsdfgsdfgdox', tag: '3332', money: '123', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: '붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸붸', tag: '3332', money: '12344', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mindfgibox', tag: '3332', money: '124443', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mini', tag: '3332', money: '1423', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mdfginibox', tag: '3332', money: '1243', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' },
+        { avatar: '', name: 'Mbox', tag: '3332', money: '14444423', joinAt: '2020. 03. 05', checkAt: '2020. 03. 07' }
+      ],
+      unit: '원'
+    }
+  },
   mounted () {
     this.$store.commit('setTitle', '유저')
     this.$store.commit('setActivate', this.$route.name)
   }
 }
 </script>
+
+<style>
+#user {
+  display: flex;
+  justify-content: center;
+}
+
+#user-table {
+  border-collapse: separate;
+  border-spacing: 0 1rem;
+}
+
+.userbox {
+  background: #1D2936;
+  box-shadow: 2px 2px 2px 2px black;
+  border-radius: 15px;
+}
+
+tr td:first-child {
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
+}
+
+tr td:last-child {
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
+}
+
+#user-table th {
+  padding: .5rem;
+}
+
+#user-table td {
+  width: 100px;
+  padding: .5rem;
+}
+
+#user-table td:nth-child(1) {
+  width: auto;
+  padding-right: 1rem;
+  overflow: hidden;
+  justify-content: left;
+  align-items: center;
+}
+
+#user-table td:nth-child(2) {
+  min-width: 150px;
+  width: auto;
+}
+
+#user-table td:last-child {
+  width: 70px;
+}
+
+#user-table td:nth-child(2) { display: none }
+#user-table td:nth-child(3) { display: none }
+#user-table td:nth-child(4) { display: none }
+#user-table th:nth-child(2) { display: none }
+#user-table th:nth-child(3) { display: none }
+#user-table th:nth-child(4) { display: none }
+
+.user {
+  /*display: flex;*/
+  justify-content: center;
+}
+
+.iconbtn {
+  cursor: pointer;
+}
+
+@media ( min-width: 900px ) {
+  #user-table td:nth-child(2) { display: table-cell }
+  #user-table td:nth-child(3) { display: table-cell }
+  #user-table td:nth-child(4) { display: table-cell }
+  #user-table th:nth-child(2) { display: table-cell }
+  #user-table th:nth-child(3) { display: table-cell }
+  #user-table th:nth-child(4) { display: table-cell }
+}
+</style>
