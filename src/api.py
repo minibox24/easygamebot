64fe3da341b86387813099234f18c5e568f65833
minibox24 <minibox724@gmail.com>
2021-03-12 23:58:51 +0900

    feat(webserver): authorized decorator

--- src/api.py
+++ src/api.py
@@ -6,14 +6,56 @@ from src.utils import get_config
 from src.utils.classes import Status
 
 import asyncio
+from functools import wraps
+from base64 import b64decode
+import binascii
+
 
 api = Blueprint("api", url_prefix="/api")
+username = "admin"
 
 
-def response(status: Status, message: str = "", data=None) -> resjson:
+def response(
+    status: Status, message: str = "", data=None, status_code: int = 200
+) -> resjson:
     if data is None:
         data = {}
-    return resjson({"status": status.value, "message": message, "data": data})
+    return resjson(
+        {"status": status.value, "message": message, "data": data}, status=status_code
+    )
+
+
+def authorized():
+    def decorator(f):
+        @wraps(f)
+        async def decorated_function(request, *args, **kwargs):
+            is_auth = False
+
+            try:
+                typ, cre = request.headers.get("Authorization").split()
+
+                config = get_config()
+                if typ == "Basic":
+                    if (
+                        b64decode(cre.encode()).decode()
+                        == f'{username}:{config["admin_tool"]["password"]}'
+                    ):
+                        is_auth = True
+            except binascii.Error:
+                pass
+            except ValueError:
+                pass
+            except AttributeError:
+                pass
+            if is_auth:
+                res = await f(request, *args, **kwargs)
+                return res
+            else:
+                return response(Status.AUTH, "need authorization", status_code=401)
+
+        return decorated_function
+
+    return decorator
 
 
 @api.route("/")
